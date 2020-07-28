fd065c5f0e288ac57884eff877eeb2bbde5ba302
Muhammad Osaid <muhammadosaid06@gmail.com>
2020-07-28 13:06:30 +0800

    Initial Commit

---
+++ QuickFix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickFix
+//
+//  Created by Muhammad Osaid on 28/7/20.
+//  Copyright © 2020 Muhammad Osaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
