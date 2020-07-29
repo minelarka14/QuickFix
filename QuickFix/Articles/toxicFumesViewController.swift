46af83fca1b690f79675386d0109d90485d67fc8
Muhammad Osaid <muhammadosaid06@gmail.com>
2020-07-29 09:46:50 +0800

    Setup fro toxic fumes VC

---
+++ QuickFix/Articles/toxicFumesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  toxicFumesViewController.swift
+//  QuickFix
+//
+//  Created by Muhammad Osaid on 29/7/20.
+//  Copyright © 2020 Muhammad Osaid. All rights reserved.
+//
+
+import UIKit
+
+class toxicFumesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
