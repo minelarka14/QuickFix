cf3121a27bdd4e594dddf80a3aed6ae8254ce6cc
Muhammad Osaid <muhammadosaid06@gmail.com>
2020-08-04 13:27:30 +0800

    Remove unneeded helpers

--- QuickFix/Helpers/Utilities.swift
+++ QuickFix/Helpers/Utilities.swift
@@ -10,32 +10,9 @@ import Foundation
 import UIKit
 
 class Utilities {
-    
-    static func styleTextField(_ textfield:UITextField) {
-        let bottomLine = CALayer()
-        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 2)
-        bottomLine.backgroundColor = UIColor.init(red: 255/255, green: 0/255, blue: 0/255, alpha: 1).cgColor
-        textfield.borderStyle = .none
-        textfield.layer.addSublayer(bottomLine)
-    }
-    
     static func styleFilledButton(_ button:UIButton) {
         button.backgroundColor = UIColor.init(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
         button.layer.cornerRadius = 25.0
         button.tintColor = UIColor.white
     }
-    
-    static func styleHollowButton(_ button:UIButton) {
-        button.layer.borderWidth = 2
-        button.layer.borderColor = UIColor.black.cgColor
-        button.layer.cornerRadius = 25.0
-        button.tintColor = UIColor.black
-    }
-    
-    static func isPasswordValid(_ password : String) -> Bool {
-        
-        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
-        return passwordTest.evaluate(with: password)
-    }
-    
 }
